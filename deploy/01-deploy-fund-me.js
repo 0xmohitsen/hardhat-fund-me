a7882796546eab7ec2a7b523325bc9d1e04078fb
0xmohitsen <mohitsen0102@gmail.com>
2024-11-06 23:01:37 +0530

    Deployed contract + verified contract

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -4,6 +4,8 @@ const {
     networkConfig,
     developmentChains,
 } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+require("dotenv").config();
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
@@ -25,12 +27,22 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     }
     // well what happens when we want to change chains?
     // when going for localhost or hardhat network we want to use a mock"
-
+    const args = [ethUsdPriceFeedAddress];
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [ethUsdPriceFeedAddress], // put price feed address
+        args: args, // put price feed address
         log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
     });
+
+    log(`FundMe deployed at ${fundMe.address}`);
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args);
+    }
 };
 
 module.exports.tags = ["all", "fundme"];
--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -18,6 +20,7 @@ module.exports = {
             url: SEPOLIA_RPC_URL,
             accounts: [PRIVATE_KEY],
             chainId: 11155111,
+            blockConfirmations: 6,
         },
     },
     // solidity: "0.8.27",
@@ -33,9 +36,29 @@ module.exports = {
                 version: "0.6.0",
             },
             {
+                version: "0.8.17",
+            },
+            {
+                version: "0.8.18",
+            },
+            {
+                version: "0.8.19",
+            },
+            {
                 version: "0.8.27",
             },
         ],
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+        // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: COINMARKETCAP_API_KEY,
     },
     namedAccounts: {
         deployer: {
